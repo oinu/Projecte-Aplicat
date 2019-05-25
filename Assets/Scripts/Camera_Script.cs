b0528ee6d2c3dff7259471d3132c5517be974d9b
oinu <oinu69@gmail.com>
2019-05-25 12:21:45 +0200

    S'ha afegit la mecanica de teletransportarse. El circuit del barri pobre es funcional.
    Signed-off-by: oinu <oinu69@gmail.com>

--- Assets/Scripts/Camera_Script.cs
+++ Assets/Scripts/Camera_Script.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class Camera_Script : MonoBehaviour {
 
 	public GameObject character;
-	public float angleVelocity;
-	private GameObject obj;
+	private float angleVelocity;
+	private GameObject obj,rayObject;
 	private Vector3 position;
 	private float turnPosition;
-
 
 	public GameObject GetObj
 	{
@@ -34,6 +33,12 @@ public class Camera_Script : MonoBehaviour {
 		this.transform.parent=obj.transform;
 		turnPosition=0;
 
+		rayObject= new GameObject("RayCastObject");
+		rayObject.transform.parent=obj.transform;
+		rayObject.transform.position= new Vector3(character.transform.position.x,
+		character.transform.position.y+character.transform.localScale.y,character.transform.position.z);
+
+
 	}
 	
 	// Update is called once per frame
@@ -45,6 +50,23 @@ public class Camera_Script : MonoBehaviour {
 		}	
 
 		obj.transform.position=character.transform.position;
+
+		rayObject.transform.position= new Vector3(character.transform.position.x,
+		character.transform.position.y+character.transform.localScale.y+0.5f,character.transform.position.z);
+		rayObject.transform.forward= obj.transform.forward;
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			if(Physics.Raycast(rayObject.transform.position,rayObject.transform.forward,out hit))
+			{
+				if(hit.transform.tag=="Transportar")
+				{
+					character.GetComponent<Character_Script>().SetTeleportGoal(hit.point);
+					Destroy(hit.transform.gameObject);
+				}
+			}
+		}
 
 	}
 }
--- Assets/Scripts/Character_Script.cs
+++ Assets/Scripts/Character_Script.cs
@@ -7,7 +7,7 @@ public class Character_Script : MonoBehaviour {
 	public GameObject currenCamera;
 	public float velocity,jumpForce;
 	private Vector3 position,direction;
-	private bool inCollision,inAir,swipeForward;
+	private bool inCollision,inAir,teleport;
 	private GameObject collisionObject,collisionInstance;
 	private Transform objTrans;
 	// Use this for initialization
@@ -15,40 +15,44 @@ public class Character_Script : MonoBehaviour {
 		position= this.transform.position;
 		collisionInstance= new GameObject();
 		collisionInstance.transform.position = new Vector3(0f,10000f,0f);
-		swipeForward=false;
+		teleport=false;
 		objTrans= currenCamera.GetComponent<Camera_Script>().GetObj.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		#region Movement
-		position=this.transform.position;
+		if(!teleport)
+		{
+			position=this.transform.position;
 
-		if(Input.GetAxis("Vertical")>0)
-		{
-			if(inCollision && this.transform.position.z>collisionObject.transform.position.z)inCollision=false;
-			position+=objTrans.forward.normalized*velocity;
-		}
+			if(Input.GetAxis("Vertical")>0)
+			{
+				if(inCollision && this.transform.position.z>collisionObject.transform.position.z)inCollision=false;
+				position+=objTrans.forward.normalized*velocity;
+			}
 
-		if(!inCollision && !Vector3.Equals(this.transform.position,position))
-		{
-			direction=position-this.transform.position;
-			this.transform.forward=direction.normalized;
-			this.transform.position+=direction;
-		}
-		#endregion
+			if(!inCollision && !Vector3.Equals(this.transform.position,position))
+			{
+				direction=position-this.transform.position;
+				this.transform.forward=direction.normalized;
+				this.transform.position+=direction;
+			}
+			#endregion
 
-		if(!inAir && Input.GetAxis("Jump")!=0)
-		{
-			this.GetComponent<Rigidbody>().AddForce(0,jumpForce,0,ForceMode.Impulse);
-			inAir=true;
+			if(!inAir && Input.GetAxis("Jump")!=0)
+			{
+				this.GetComponent<Rigidbody>().AddForce(0,jumpForce,0,ForceMode.Impulse);
+				inAir=true;
+			}
+			else if (inAir)
+			{
+				float collPoint=collisionObject.transform.position.y+ collisionObject.transform.localScale.y/2;
+				float thisPoint= transform.position.y-transform.localScale.y/2;
+				if(inCollision)inCollision=!(collPoint<thisPoint);
+			}
 		}
-		else if (inAir)
-		{
-			float collPoint=collisionObject.transform.position.y+ collisionObject.transform.localScale.y/2;
-			float thisPoint= transform.position.y-transform.localScale.y/2;
-			if(inCollision)inCollision=!(collPoint<thisPoint);
-		}
+		
 	}
 
 	private void OnCollisionEnter(Collision other) {
@@ -66,5 +70,12 @@ public class Character_Script : MonoBehaviour {
 	private void OnCollisionExit(Collision other) {
 		inCollision=false;
 
+	}
+
+	public void SetTeleportGoal(Vector3 position)
+	{
+		teleport= true;
+		this.transform.position= position;
+		teleport= false;
 	}
 }
